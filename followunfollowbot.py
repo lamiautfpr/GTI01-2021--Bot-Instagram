037d71619925fef6e3db1e02ea3c553d906dea62
João Pacheco <55768838+joaopacheco7@users.noreply.github.com>
2021-06-09 12:22:53 -0300

    Create followunfollowbot.py

---
+++ followunfollowbot.py
@@ -0,0 +1,25 @@
+from instapy import InstaPy
+from instapy import smart_run
+
+session = InstaPy(username="example", 
+                  password="example")
+
+with smart_run(session):
+  
+  session.follow_user_followers(['filipedeschamps'], amount=500,
+                                randomize=False, interact=False)
+  
+  session.unfollow_users(amount=500, InstapyFollowed=(True, "nonfollowers"),
+                        style="FIFO",
+                        unfollow_after=12 * 60 * 60, sleep_delay=601)
+  
+  session.follow_user_followers(['filipedeschamps'], amount=500,
+                                randomize=False, interact=False)
+    
+  session.unfollow_users(amount=500, InstapyFollowed=(True, "nonfollowers"),
+                          style="FIFO",
+                          unfollow_after=12 * 60 * 60, sleep_delay=601)
+  
+  session.unfollow_users(amount=500, InstapyFollowed=(True, "all"),
+                        style="FIFO", unfollow_after=24 * 60 * 60,
+                        sleep_delay=601)
